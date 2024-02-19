5fd081dadccb3cd3b37c6112a83858e9bbb49ddf
Sidit Srivastava <siditsrivastava84@gmail.com>
2024-02-20 02:32:57 +0530

    javascript_File

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,3 @@
+"use strict"  // treate all JS Code as newer version  
+
+// alert(3 +3)  we using a node js , not a browser
---
+++ 01_basics/variable.js
@@ -0,0 +1,17 @@
+const accountId = 133461;
+let accountEmail = "sidit@gmail.com";
+var password = "13641";
+
+// var accountstate;  in this line developer declears the not assign it 
+// so the javascipt show a undefined
+
+// accountCity = "orai" but it not a good.
+
+// accountId = 2 not to change
+
+// not use a var keywords because of issue scope and functional scope 
+
+accountEmail = "ghd@gmail.com";
+password = "554545";
+
+console.table([accountId, accountEmail, password , accountstate]);
