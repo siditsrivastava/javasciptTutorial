8eb586db3a1b715a04de959fab3dd3459129de90
Sidit Srivastava <siditsrivastava84@gmail.com>
2024-02-20 18:46:10 +0530

    updatednewFiles

--- 01_basics/datatypes.js
+++ 01_basics/datatypes.js
@@ -1,3 +1,47 @@
-"use strict"  // treate all JS Code as newer version  
+"use strict"; // treate all JS Code as newer version
 
 // alert(3 +3)  we using a node js , not a browser
+
+// code readable is so High in javascript
+
+// STRING
+let name = "sidit";
+
+//  NUMBER
+let phoneNumber = 1256434;
+
+// bIGINT
+let totalMemeber = 1425536516n;
+
+// BOOLEAN
+let checkBox = "true"; //  true = 1 ; false = 2;
+
+// NULL
+let middleName = null; // It means the value is decleare and null is a standalone value . null and Null is Different
+
+// UNDERFINED
+let emailId; //  It means that value is defined but value is not decleared.
+
+// ARRAY
+let userHobby = ["Cricket , Hockey , Chess"]; // Arrayv Store a multiple value like String and number , It is changeable
+
+// Objects
+let empolyeeData = [
+    {
+        name: "Sidit",
+        phoneNumbver: 4564565,
+        totalMemeber : 4664435116464n,
+        checkBox : false,
+        middleName:null,
+        
+      },
+
+      {
+        name: "Sidit Srivastava",
+        phoneNumbver: 64565,
+        totalMemeber : 465116464n,
+        checkBox : ture,
+        middleName:null,
+        
+      },
+];
